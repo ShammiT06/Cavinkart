5c1cc43ec4c11dbb57f93855c45626b7cfde16df
ShammiT06 <shammikumar0601@gmail.com>
2025-04-02 15:32:23 +0530

    Second Commit

--- src/Pages/Terms.jsx
+++ src/Pages/Terms.jsx
@@ -6,10 +6,10 @@ function Terms()
     <div className="mt-[80px]">
     <Spinz/>
     <h1 className="text-[22px] font-open p-5" style={{fontWeight:"700"}}>Terms of Service</h1>
-    <div className="text-[13px]">
-    <p className="font-inter p-5 indent-12">By requesting a Cavinkart Spinz product sample, you agree to the following terms and conditions. This offer is valid only for individuals residing in India who are 18 years or older. Each participant is eligible for only one sample, subject to availability and while stocks last.</p>
-    <p className="font-inter p-5 indent-12"> Cavinkart reserves the right to modify, extend, or cancel the offer at any time without prior notice. The sample is intended for personal use only and must not be sold or distributed for commercial purposes. Delivery timelines may vary, and Cavinkart is not responsible for any delays, incorrect addresses, or failed deliveries due to unforeseen circumstances.</p>
-    <p className="font-inter p-5 indent-12">By participating, you consent to Cavinkart collecting and using your personal information for processing your request and future promotional communication, as per our Privacy Policy. You may opt out of such communications at any time. Cavinkart makes no warranties regarding the suitability of the product, and users are advised to check ingredients for potential allergens before use. Cavinkart shall not be held liable for any reactions, damages, or losses arising from the use of the sample. In case of disputes, Cavinkart’s decision shall be final and binding.</p>
+    <div className="text-[13px] font-inter p-5 indent-12">
+    <p className="mb-3">By requesting a Cavinkart Spinz product sample, you agree to the following terms and conditions. This offer is valid only for individuals residing in India who are 18 years or older. Each participant is eligible for only one sample, subject to availability and while stocks last.</p>
+    <p className="mb-3">Cavinkart reserves the right to modify, extend, or cancel the offer at any time without prior notice. The sample is intended for personal use only and must not be sold or distributed for commercial purposes. Delivery timelines may vary, and Cavinkart is not responsible for any delays, incorrect addresses, or failed deliveries due to unforeseen circumstances.</p>
+    <p className="mb-3">By participating, you consent to Cavinkart collecting and using your personal information for processing your request and future promotional communication, as per our Privacy Policy. You may opt out of such communications at any time. Cavinkart makes no warranties regarding the suitability of the product, and users are advised to check ingredients for potential allergens before use. Cavinkart shall not be held liable for any reactions, damages, or losses arising from the use of the sample. In case of disputes, Cavinkart’s decision shall be final and binding.</p>
     </div>
     </div>
     </>)
