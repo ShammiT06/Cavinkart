0ab2a704e8b1d81aaa07c2bb4bcc81ad8fed9203
ShammiT06 <shammikumar0601@gmail.com>
2025-04-02 15:26:42 +0530

    1st Comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Terms from "./Pages/Terms"
+
+function App()
+{
+  return(<>
+  <Terms/>
+
+  </>)
+
+}
+export default App
---
+++ src/Components/Spinz.jsx
@@ -0,0 +1,10 @@
+import image1 from "../assets/Spinz.png"
+function Spinz()
+{
+    return(<div className="flex justify-center items-center">
+    <img src={image1} alt="no image" className="w-[100px] h-[100px]" />
+    </div>)
+
+
+}
+export default Spinz
