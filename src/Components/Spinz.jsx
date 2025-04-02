c90e781e72234789cfeb7e7ae83e20afc1e5cf22
ShammiT06 <shammikumar0601@gmail.com>
2025-04-02 16:00:52 +0530

    updated Spinz component

--- src/Components/Spinz.jsx
+++ src/Components/Spinz.jsx
@@ -2,7 +2,7 @@ import image1 from "../assets/Spinz.png"
 function Spinz()
 {
     return(<div className="flex justify-center items-center">
-    <img src={image1} alt="no image" className="w-[100px] h-[100px]" />
+    <img src={image1} alt="no image"/>
     </div>)
 
 
