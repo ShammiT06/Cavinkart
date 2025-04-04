5b7f7816436c594ee2d774e17c7589c413757e7e
ShammiT06 <shammikumar0601@gmail.com>
2025-04-04 11:54:42 +0530

    Updated Location page

--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Reusablespinz from "../Components/Reusablespinz"
 import "../Css/location.css"
 
@@ -7,15 +8,24 @@ function Location()
         <Reusablespinz/>
         <div>
             <div className="bg-opacity-60 flex justify-center items-center bg-slate-700" id="location">
-                <div className="bg-[#1E1E1EBF] w-[270px] h-[432px] text-white">
+                <div className="bg-[#1E1E1EBF] w-[270px] h-[440px] text-white rounded-[14px]">
                     <div className="location__header">
                         <h1 className="font-inter font-semibold text-[17px] indent-4">Allow “Diary” to use your <span className="ml-20">location?</span></h1>
                         <p className="text-[13px] font-medium">Turning on location services allows us <span className="ml-3">to show you when pals are nearby.</span></p>
                     </div>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248756.13131228785!2d80.04385969043575!3d13.047473316234447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5265ea4f7d3361%3A0x6e61a70b6863d433!2sChennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1743676407800!5m2!1sen!2sin" width="270" height="180" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4013467.1025377945!2d73.26350234190727!3d10.78053209201264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b00c582b1189633%3A0x559475cc463361f0!2sTamil%20Nadu!5e0!3m2!1sen!2sin!4v1743676861541!5m2!1sen!2sin" width="270" height="180"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <div className="flex flex-col">
+                <button className="w-[270px] h-[44px] border border-[#787878] font-inter text-[#5A91F7]" style={{padding:"11px 8px"}}>Allow</button>
+                <button className="w-[270px] h-[44px] border border-[#787878] text-[#5A91F7]" style={{padding:"11px 8px"}}>Allow While Using App</button>
+                <button className="w-[270px] h-[44px] border border-[#787878] text-[#5A91F7] rounded-bl-xl rounded-br-xl" style={{padding:"11px 8px"}}><Link to={"/cam"}>Don’t Allow</Link></button>
                 </div>
+                </div>
+
+                
             </div>
+        
         </div>
+        
     </div>)
 
 
